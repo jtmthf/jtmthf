952b15894473af10561222b90cece980798a1322
Jack Moore <8162598+jtmthf@users.noreply.github.com>
2026-02-28 21:36:20 -0500

    upgrade to next 16

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,6 @@
 // @ts-check
 
 import createMDX from '@next/mdx';
-import rehypeShikiFromHighlighter from '@shikijs/rehype/core';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-import rehypeSlug from 'rehype-slug';
-import remarkFrontmatter from 'remark-frontmatter';
-import remarkGfm from 'remark-gfm';
-import { createHighlighterCore } from 'shiki/core';
-import { createOnigurumaEngine } from 'shiki/engine/oniguruma';
-
-const highlighter = await createHighlighterCore({
-  themes: [
-    import('@shikijs/themes/github-light'),
-    import('@shikijs/themes/github-dark'),
-  ],
-  langs: [
-    import('@shikijs/langs/json'),
-    import('@shikijs/langs/sh'),
-    import('@shikijs/langs/tsx'),
-    import('@shikijs/langs/typescript'),
-  ],
-  engine: createOnigurumaEngine(() => import('shiki/wasm')),
-});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -33,20 +12,11 @@ const nextConfig = {
 
 const withMDX = createMDX({
   options: {
-    remarkPlugins: [remarkFrontmatter, remarkGfm],
+    remarkPlugins: ['remark-gfm', 'remark-frontmatter'],
     rehypePlugins: [
-      rehypeSlug,
-      [rehypeAutolinkHeadings, { behavior: 'wrap' }],
-      [
-        rehypeShikiFromHighlighter,
-        highlighter,
-        {
-          themes: {
-            light: 'github-light',
-            dark: 'github-dark',
-          },
-        },
-      ],
+      'rehype-slug',
+      ['rehype-autolink-headings', { behavior: 'wrap' }],
+      ['rehype-pretty-code', { theme: 'github-dark' }],
     ],
   },
 });
